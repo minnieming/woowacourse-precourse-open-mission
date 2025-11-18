55d3df86cac875cd2228d675a6db7d43b72e53e0
LeeEunmi <eunmilee9109.1@gmail.com>
2025-11-18 20:49:56 +0900

    feat(gradle): build.gradle에 필요한 gradle 추가 및 기본 세팅
    기본 gradle 설정, 버전, 의존성 추가, 테스트 환경 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,33 @@
 plugins {
-    kotlin("jvm") version "1.9.21"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktor)
+    alias(libs.plugins.kotlin.serialization)
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.example"
+version = "0.0.1"
 
-repositories {
-    mavenCentral()
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
 }
 
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    implementation(libs.ktor.server.core)
+    implementation(libs.ktor.server.swagger)
+    implementation(libs.ktor.server.netty)
+    implementation(libs.logback.classic)
+    implementation(libs.ktor.server.config.yaml)
+    implementation("io.ktor:ktor-server-content-negotiation:3.3.1")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.3.1")
+
+    testImplementation(platform(libs.junit.bom))
+    testImplementation(libs.ktor.server.test.host)
+    testImplementation(libs.kotlin.test.junit5)
+    testImplementation(libs.junit.jupiter)
+    testImplementation("io.ktor:ktor-client-content-negotiation:3.3.1")
+    testImplementation("io.ktor:ktor-serialization-kotlinx-json:3.3.1")
 }
 
 tasks.test {
     useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(17)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,7 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
-}
 rootProject.name = "open-mission-kotllin"
 
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
